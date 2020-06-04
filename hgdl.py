dce9b95b2ebefffe9c3cd3c1e0fb3087df6e6bc6
Elliott Perryman <elliottperryman@vivaldi.net>
2020-06-04 13:09:20 -0500

    removed verbosity - seems to work!

--- hgdl.py
+++ hgdl.py
@@ -106,7 +106,6 @@ def HGDL(func, grad, hess, bounds, r=.3, alpha=.1, maxEpochs=5, numIndividuals=5
     if numWorkers is None: numWorkers = max(cpu_count(logical=False)-1,1)
     workers = Pool(processes=numWorkers)
     for i in range(maxEpochs):
-        print(starts.shape, func_vals.shape)
         newStarts = GeneticStep(starts, func_vals, bounds)
         newFuncVals = np.array([func(x) for x in newStarts])
         starts = np.append(starts, newStarts, 0)
