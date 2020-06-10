8bea560269bbd097dba8706e64ff7731bd2507fa
Elliott Perryman <elliottperryman@vivaldi.net>
2020-06-10 17:46:55 -0500

    changed genetic algo to include old people

--- hgdl.py
+++ hgdl.py
@@ -16,15 +16,15 @@ def random_sample(N,k,bounds):
     sample += bounds[:,0]
     return sample
 
-def alreadyFound(newMinima, oldMinima, radius_squared, k):
+def alreadyFound(newMinima, oldMinima, radius_squared):
     """
     check if the new minimum is within range of the old ones
     """
-    c = oldMinima[:,:k] - newMinima[0,:k]
+    c = oldMinima - newMinima
     return (np.sum(c*c,1)<radius_squared).any()
 
 # This is my implementation of a genetic algorithm
-def GeneticStep(X, y, bounds):
+def GeneticStep(X, y, bounds, numChoose):
     """
     Input:
     X is the individuals - points on a surface
@@ -55,22 +55,24 @@ def GeneticStep(X, y, bounds):
     p /= np.sum(p)
     if np.isnan(p).any():
         raise Exception("got isnans in GeneticStep")
-    moms = np.random.choice(N, N, replace=True, p=p)
-    dads = np.random.choice(N, N, replace=True, p=p)
+    moms = np.random.choice(N, size=numChoose, replace=True, p=p)
+    dads = np.random.choice(N, size=numChoose, replace=True, p=p)
     # calculate a perturbation to the median
     #   of each individual's parents
     perturbation = np.random.normal(
             loc = 0.,
             scale=wildness*(bounds[:,1]-bounds[:,0]),
-            size=(N,k))
+            size=(numChoose,k))
     # the children are the median of their parents plus a perturbation
     norm = p[moms]+p[dads]
     weights = (p[moms]/norm, p[dads]/norm)
     weighted_linear_sum = weights[0].reshape(-1,1)*X[moms] + weights[0].reshape(-1,1)*X[dads]
     children = weighted_linear_sum + perturbation
+    oob = np.logical_not([in_bounds(x,bounds) for x in children])
+    children[oob] = random_sample(np.sum(oob), k, bounds)
     return children
 
-def deflated_local(starts, results_all, results_minima, gradient, hessian, bounds, workers,r,alpha, maxLocal):
+def deflated_local(starts, results_edge, results_minima, gradient, hessian, bounds, workers,r,alpha, maxLocal):
     for j in range(maxLocal):
         percent_none = 0.
         tmp_results = workers.imap_unordered(
@@ -80,17 +82,16 @@ def deflated_local(starts, results_all, results_minima, gradient, hessian, bound
             if not x["success"]:
                 percent_none += 1./starts.shape[0]
             else:
-                if alreadyFound(x["x"].reshape(1,-1), results_all, r**2, starts.shape[1]):
+                if alreadyFound(x["x"], results_edge, r**2) or alreadyFound(x["x"], results_minima, r**2):
                     percent_none += 1./starts.shape[0]
                 else:
                     if x["edge"]:
-                        results_all = np.append(results_all, x["x"].reshape(1,-1), 0)
+                        results_edge = np.append(results_edge, x["x"].reshape(1,-1), 0)
                     else:
-                        results_all = np.append(results_all, x["x"].reshape(1,-1), 0)
                         results_minima = np.append(results_minima, x["x"].reshape(1,-1), 0)
             if percent_none > 0.2:
-                return results_all, results_minima
-    return results_all, results_minima
+                return results_edge, results_minima
+    return results_edge, results_minima
 
 def HGDL(func, grad, hess, bounds, r=.3, alpha=.1, maxEpochs=5, numIndividuals=5, maxLocal=5, numWorkers=None, bestX=5):
     """
@@ -119,37 +120,36 @@ def HGDL(func, grad, hess, bounds, r=.3, alpha=.1, maxEpochs=5, numIndividuals=5
         or {"success":True, "x",x, "y",y} with the bestX x's and their y's
     """
     k = len(bounds)
-    starts = random_sample(numIndividuals, k, bounds)
-    func_vals = np.array([func(x) for x in starts])
-    results_all = np.empty((0,k))
+    genetic_x = random_sample(numIndividuals, k, bounds)
+    genetic_y = np.array([func(x) for x in genetic_x])
+    results_edge = np.empty((0,k))
     results_minima = np.empty((0,k))
     if numWorkers is None: numWorkers = max(cpu_count(logical=False)-1,1)
     workers = Pool(processes=numWorkers)
     for i in range(maxEpochs):
-        newStarts = GeneticStep(starts, func_vals, bounds)
+        newStarts = GeneticStep(genetic_x, genetic_y, bounds, numIndividuals)
         newFuncVals = np.array([func(x) for x in newStarts])
-        starts = np.append(starts, newStarts, 0)
-        func_vals = np.append(func_vals, newFuncVals)
-        c = np.argsort(func_vals)
-        starts, func_vals = starts[c], func_vals[c]
-#        if i%2==1:
-#            starts, func_vals = starts[:2*numIndividuals], func_vals[:2*numIndividuals]
-        results_all, results_minima = deflated_local(starts[:numIndividuals], results_all, results_minima, grad, hess, bounds, workers, r, alpha, maxLocal)
-        print('found:',results_all)
+        genetic_x = np.append(genetic_x, newStarts, 0)
+        genetic_y = np.append(genetic_y, newFuncVals)
+        c = np.argsort(genetic_y)
+        genetic_x, genetic_y = genetic_x, genetic_y
+        results_edge, results_minima = deflated_local(
+                genetic_x[:numIndividuals], results_edge,
+                results_minima, grad, hess, bounds, workers, r, alpha, maxLocal)
+        print("at epoch ",i+1,", found edges",results_edge.round(2),"\nminima",results_minima.round(2))
 
     workers.close()
-    func_vals_all = np.array([func(x) for x in results_all])
-    x = np.append(results_all, starts, 0)
-    y = np.append(func_vals_all, func_vals)
-    if len(x) == 0:
-        return {"success":False}
-    c = np.argsort(y)
-    if len(x) < bestX:
+    func_vals_edge = np.array([func(x) for x in results_edge])
+    func_vals_minima = np.array([func(x) for x in results_minima])
+    c_edge, c_minima = np.argsort(func_vals_edge), np.argsort(func_vals_minima)
+    results_minima, func_vals_minima = results_minima[c_minima], func_vals_minima[c_minima]
+    results_edge, func_vals_edge = results_edge[c_edge], func_vals_edge[c_edge]
+    if len(results_minima) < bestX:
         print("well there buckaroo, i couldn't find all ya asked for, my guy")
-        return {"success":True,"x":x,"func":y}
-    x, y = x[c][:bestX], y[c][:bestX]
-    return {"success":True,"x":x,"func":y}
-
+    return {
+            "minima":results_minima, "minima_y":func_vals_minima,
+            "edge":results_edge, "edge_y": func_vals_edge,
+            "genetic":genetic_x, "genetic_y":genetic_y}
 #from scipy.optimize import rosen, rosen_der, rosen_hess
 #b = np.array([[-20, 20],[-30,30.]])
 #HGDL(rosen, rosen_der, rosen_hess, b)
