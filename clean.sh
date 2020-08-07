b2862ef08d9d1589253fe19b3153450e54437e23
Elliott Perryman <elliottperryman@vivaldi.net>
2020-08-07 17:11:05 -0500

    now cleans recursively

--- clean.sh
+++ clean.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-rm -rf hgdl.egg-info build dist dask-worker-space
+rm -rf hgdl.egg-info build dist
+find . -type d -name '__pycache__' -exec rm -r {} +
+find . -type d -name '.ipynb_checkpoints' -exec rm -r {} +
+find . -type d -name 'dask-worker-space' -exec rm -r {} +
