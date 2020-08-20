40bea62fe2fa74771a41c99db886bbf5c58eb997
Marcus Michael Noack <MarcusNoack@lbl.gov>
2020-08-20 15:14:02 -0700

    dNewton will go into a gradient descent which approaches the boarder if it jumps out

--- hgdl/local_methods/dNewton.py
+++ hgdl/local_methods/dNewton.py
@@ -3,17 +3,11 @@ import hgdl.misc as misc
 import hgdl.deflation.bump_function as defl
 
 def DNewton(func, grad, hess, x0,x_defl,bounds,radius,max_iter = 20, args = ()):
-    #w = 0 #0.1 #np.random.rand() * 10.0
-    #print("DNewton from point:", x0," started, waits: ", w)
-    #time.sleep(w)
     e = np.inf
     success = True
     counter = 0
     tol = 1e-6
     x = np.array(x0)
-    #print("==================")
-    #print(x_defl)
-    #print("-------")
     while e > tol:
         counter += 1
         d = defl.deflation_function(x,x_defl,radius)
@@ -24,22 +18,19 @@ def DNewton(func, grad, hess, x0,x_defl,bounds,radius,max_iter = 20, args = ()):
         try:
             gamma = np.linalg.solve(hessian + (np.outer(gradient,dg)/d),-gradient)
         except Exception as error: 
-            #print("solve in dNewton crashed because: ",str(error)," starting least squares")
-            #print(hessian)
             gamma,a,b,c = np.linalg.lstsq(hessian + (np.outer(gradient,dg)/d),-gradient)
         x += gamma
         if counter >= max_iter or misc.out_of_bounds(x,bounds):
-            #print("DNewton from point:", x0," not converged  ",x," ", counter, misc.out_of_bounds(x,bounds))
-            return x0,func(x0, *args),e,np.linalg.eig(hess(x0, *args))[0],False
-
-        #print("current position: ",x,"epsilon: ",e, gamma, "d*grad: ",d,gradient, "hess ",hessian)
-    #print("DNewton from point:", x0," converged to ",x, " with ",e," after waiting: ",w)
-    #input()
+            x,f, s = gradient_descent(func,grad,bounds,x_defl,x-gamma,radius,args)
+            return x,f,e,np.linalg.eig(hess(x, *args))[0],s
+            #return x0,func(x0, *args),e,np.linalg.eig(hess(x0, *args))[0],False
     return x,func(x, *args),e,np.linalg.eig(hessian)[0], success
 
 
-def gradient_descent(ObjectiveFunction, GradientFunction,bounds,x0, radius, *args):
+
+def gradient_descent(ObjectiveFunction, GradientFunction,bounds,x_defl,x0, radius, args):
     dim = len(bounds)
+    bounds = np.array(bounds)
     epsilon = np.inf
     step_counter = 0
     beta = 0.8
@@ -51,16 +42,16 @@ def gradient_descent(ObjectiveFunction, GradientFunction,bounds,x0, radius, *arg
         gradient = GradientFunction(x, *args) * d
         counter = 0
         step = 1.0
-        while OutOfBounds(x + (step * gradient), ParameterLimits) or \
+        while misc.out_of_bounds(x - (step * gradient), bounds) or \
               ObjectiveFunction(x - (step * gradient), *args) > \
-              ObjectiveFunction(x, *args) - \
-              ((step / 2.0) * np.linalg.norm(gradient) ** 2):
+              ObjectiveFunction(x, *args) - ((step / 2.0) * np.linalg.norm(gradient) ** 2):
             step = step * beta
             counter += 1
-            if counter > 100:
+            if counter > 10:
                 break
-        x = x + (step * gradient)
-        epsilon = np.linalg.norm(step * gradient)
+        x = x - (step * gradient)
+        #print(step_counter,x,gradient)
+        epsilon = np.linalg.norm(gradient)
         if step_counter > 100:
             success = False
             break
