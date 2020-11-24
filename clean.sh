e1ff86c45c7bc9b5dcf552c6eeceec0eb6d0444f
Elliott Perryman <elliottperryman@vivaldi.net>
2020-11-24 11:26:33 -0500

    now rm's dask locks

--- clean.sh
+++ clean.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -rf hgdl.egg-info build dist
 find . -type d -name '__pycache__' -exec rm -r {} +
+find . -type f -name '*.lock' -exec rm -r {} +
 find . -type d -name '.ipynb_checkpoints' -exec rm -r {} +
 find . -type d -name 'dask-worker-space' -exec rm -r {} +
