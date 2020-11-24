ef55d343a065b08620864f8b422270e4e00e8f11
Elliott Perryman <elliottperryman@vivaldi.net>
2020-11-24 11:50:10 -0500

    rm scheduler.json

--- clean.sh
+++ clean.sh
@@ -2,5 +2,6 @@
 rm -rf hgdl.egg-info build dist
 find . -type d -name '__pycache__' -exec rm -r {} +
 find . -type f -name '*.lock' -exec rm -r {} +
+find . -type f -name 'scheduler.json' -exec rm -r {} +
 find . -type d -name '.ipynb_checkpoints' -exec rm -r {} +
 find . -type d -name 'dask-worker-space' -exec rm -r {} +
