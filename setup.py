3bdbe73ae978d4b9f088ae5c9fc7a64825f4991d
Marcus Michael Noack <MarcusNoack@lbl.gov>
2021-07-19 12:50:08 -0700

    version 1.1.5

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open(path.join(here, 'requirements.txt')) as requirements_file:
 
 setup(
     name='hgdl',
-    version="1.1.4",
+    version="1.1.5",
     #cmdclass=versioneer.get_cmdclass(),
     description="HGDL Optimization",
     long_description=readme,
