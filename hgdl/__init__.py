6d9f2762760de15f891339f30d0b3450ce879892
Ian Humphrey <ian.r.humphrey@gmail.com>
2021-12-07 15:37:08 -0800

    remove outdated version comments in hdgl/__init__.py

--- hgdl/__init__.py
+++ hgdl/__init__.py
@@ -1,9 +1,2 @@
-#from ._version import get_versions
-
-#__version__ = get_versions()["version"]
-#del get_versions
-
-#global_config = None
-
 from . import _version
 __version__ = _version.get_versions()['version']
