7c77a6a55aab33297021a79ce76ddc08e90d613e
Ronald Pandolfi <ronpandolfi@gmail.com>
2023-05-08 16:36:57 -0700

    fix docstring title

--- hgdl/constraints.py
+++ hgdl/constraints.py
@@ -1,28 +1,28 @@
 import numpy as np
 
 class NonLinearConstraint:
-    """
-    This class provides HGDL with the capability of constrained function
-    optimization. Every constraint should be fined by one class instance.
-
-    Parameters:
-    -----------
-    nlc : Callable
-        The function of the constraint.
-    nlc_grad : Callable
-        The gradient of the constraint.
-    nlc_hess : Callable, optional
-        The Hessian of the constraint, if available.
-        Default = None. In this case it will be approximated.
-        If no Hessian is available, don't give HGDL the Hessian of the objective function.
-    ctype : str, optional
-        String '<', '>', or '=', defining the constraint as inequality or equalit constraint.
-        Default = '='.
-    values : float, optional
-        Value of the constraint. Default = 0.0.
-    """
-
     def __init__(self,nlc,nlc_grad, nlc_hess = None, ctype = "=", value = 0.0, bounds = np.array([[-1e6,1e6]])):
+        """
+        This class provides HGDL with the capability of constrained function
+        optimization. Every constraint should be fined by one class instance.
+
+        Parameters
+        ----------
+        nlc : Callable
+            The function of the constraint.
+        nlc_grad : Callable
+            The gradient of the constraint.
+        nlc_hess : Callable, optional
+            The Hessian of the constraint, if available.
+            Default = None. In this case it will be approximated.
+            If no Hessian is available, don't give HGDL the Hessian of the objective function.
+        ctype : str, optional
+            String '<', '>', or '=', defining the constraint as inequality or equalit constraint.
+            Default = '='.
+        values : float, optional
+            Value of the constraint. Default = 0.0.
+        """
+
         self.nlc = nlc
         self.nlc_grad = nlc_grad
         self.nlc_hess = nlc_hess
